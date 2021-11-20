453e2abe13b92d26acd470498a6fa4702be6dc6a
Frank Tseng <b19930813@gmail.com>
2021-11-20 21:49:03 +0800

    Revise User Controller

---
+++ Server/shop-server/shop-server/Controllers/UserController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using shop_server.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop_server.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+
+        private readonly ShopContext _context;
+
+        public UserController(ShopContext context)
+        {
+            _context = context;
+
+        }
+
+        // GET: api/Customs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        //刪除使用者資料
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> DeleteUser(int UserId)
+        {
+            var user = await _context.Users.FindAsync(UserId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        //修改使用者資料
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUesr(int UserId , User user) 
+        {
+             if(UserId != user.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(UserId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            //Create myCustom to insert
+
+            _context.Users.Add(user);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("PostUser", new { id = user.UserId }, user);
+        }
+
+        private bool UserExists(int UserId)
+        {
+            return _context.Users.Any(u => u.UserId == UserId);
+        }
+    }
+}
